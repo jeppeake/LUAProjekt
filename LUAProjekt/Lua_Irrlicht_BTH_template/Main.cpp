6424a55be488898fd4f6ceb6def4ed88b0c5aaf3
knas_man <ake_owns96@live.se>
2018-05-17 11:05:37 +0200

    initial commit

---
+++ LUAProjekt/Lua_Irrlicht_BTH_template/Main.cpp
@@ -0,0 +1,118 @@
+#pragma comment(lib, "Irrlicht.lib")
+#ifdef _DEBUG
+#pragma comment(lib, "LuaLibd.lib")
+#else
+#pragma comment(lib, "Lualib.lib")
+#endif
+
+#include <stdio.h>
+
+#include <lua.hpp>
+#include <Windows.h>
+#include <iostream>
+#include <thread>
+#include "lua.hpp"
+#include <irrlicht.h>
+
+irr::scene::IAnimatedMeshSceneNode* node;
+
+static int updatepos(lua_State* L) {
+	if (lua_gettop(L) != 3) {
+		luaL_loadstring(L, "print('Wrong number of parameters, use this format: updatepos(x,y,z)')");
+		lua_pcall(L, 0, 0, 0);
+		lua_pop(L, 1);
+		return 0;
+	}
+	
+	float x = std::atof(lua_tostring(L, -3));
+	float y = std::atof(lua_tostring(L, -2));
+	float z = std::atof(lua_tostring(L, -1));
+	node->setPosition(irr::core::vector3df(x, y, z));
+	return 0;
+}
+
+static int getpos(lua_State* L) {
+	irr::core::vector3df pos = node->getPosition();
+	lua_newtable(L);
+	lua_pushnumber(L, pos.X);
+	lua_rawseti(L, -2, 1);
+	lua_pushnumber(L, pos.Y);
+	lua_rawseti(L, -2, 2);
+	lua_pushnumber(L, pos.Z);
+	lua_rawseti(L, -2, 3);
+	return 1;
+}
+
+void registerLuaFunctions(lua_State* L) {
+	lua_pushcfunction(L, updatepos);
+	lua_setglobal(L, "updatepos");
+	lua_pushcfunction(L, getpos);
+	lua_setglobal(L, "getpos");
+}
+
+
+void ConsoleThread(lua_State* L) {
+	char command[1000];
+	while(GetConsoleWindow()) {
+		memset(command, 0, 1000);
+		std::cin.getline(command, 1000);
+		if (luaL_loadstring(L, command) || lua_pcall(L, 0, 0, 0)) {
+			std::cout << lua_tostring(L, -1) << '\n';
+			lua_pop(L, 1);
+		}
+	}
+}
+
+int main()
+{
+	lua_State* L = luaL_newstate();
+	luaL_openlibs(L);
+
+		std::thread conThread(ConsoleThread, L);
+
+	irr::IrrlichtDevice* device = irr::createDevice(irr::video::EDT_SOFTWARE, irr::core::dimension2d<irr::u32>(640, 480), 16, false, false, true, 0);
+	if(!device)
+		return 1;
+
+	device->setWindowCaption(L"Hello World! - Irrlicht Engine Demo");
+	irr::video::IVideoDriver* driver	= device->getVideoDriver();
+	irr::scene::ISceneManager* smgr		= device->getSceneManager();
+	irr::gui::IGUIEnvironment* guienv	= device->getGUIEnvironment();
+
+	guienv->addStaticText(L"Hello World! This is the Irrlicht Software renderer!", irr::core::rect<irr::s32>(10, 10, 260, 22), true);
+
+	irr::scene::IAnimatedMesh* mesh = smgr->getMesh("../../Bin/Meshes/sydney.md2");
+	if (!mesh)
+	{
+		device->drop();
+		return 1;
+	}
+	 node = smgr->addAnimatedMeshSceneNode(mesh);
+
+	if (node)
+	{
+		node->setMaterialFlag(irr::video::EMF_LIGHTING, false);
+		node->setMD2Animation(irr::scene::EMAT_STAND);
+		node->setMaterialTexture(0, driver->getTexture("../../Bin/Meshes/sydney.bmp"));
+	}
+
+	node->setScale(irr::core::vector3df(0.5f, 0.5f, 0.5f));
+
+	smgr->addCameraSceneNode(0, irr::core::vector3df(0, 30, -40), irr::core::vector3df(0, 5, 0));
+
+	registerLuaFunctions(L);
+
+	while(device->run()) {
+		driver->beginScene(true, true, irr::video::SColor(255, 90, 101, 140));
+
+		smgr->drawAll();
+		guienv->drawAll();
+
+		driver->endScene();		
+	}
+
+	device->drop();
+
+	conThread.join();
+	return 0;
+}
